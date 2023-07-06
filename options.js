cabea557390637fe0363612af333906e1900be1c
a-kobori <89172056+a-kobori@users.noreply.github.com>
2023-07-06 15:33:55 +0900

    第一弾を作成

--- options.js
+++ options.js
@@ -1,16 +1,28 @@
 /** @format */
 
 document.getElementById("save").addEventListener("click", function () {
-  console.log("Save button clicked");
-  let userText = document.getElementById("slackBotToken").value;
-  chrome.storage.sync.set({ slackBotToken: userText }, function () {
-    console.log("Value is set to " + userText);
+  const slackBotToken = document.getElementById("slackBotToken").value;
+  chrome.storage.sync.set({ slackBotToken: slackBotToken }, function () {
+    console.log("Value is set to " + slackBotToken);
+  });
+  const channelToPost = document.getElementById("channelToPost").value;
+  chrome.storage.sync.set({ channelToPost: channelToPost }, function () {
+    console.log("Value is set to " + channelToPost);
+  });
+  const messagePrefix = document.getElementById("messagePrefix").value;
+  chrome.storage.sync.set({ messagePrefix: messagePrefix }, function () {
+    console.log("Value is set to " + messagePrefix);
   });
 });
 
 // オプションページが読み込まれたときに、保存された設定を反映させる
 window.onload = function () {
-  chrome.storage.sync.get(["slackBotToken"], function (result) {
-    document.getElementById("slackBotToken").value = result.slackBotToken;
-  });
+  chrome.storage.sync.get(
+    ["slackBotToken", "channelToPost", "messagePrefix"],
+    function (result) {
+      document.getElementById("slackBotToken").value = result.slackBotToken;
+      document.getElementById("channelToPost").value = result.channelToPost;
+      document.getElementById("messagePrefix").value = result.messagePrefix;
+    }
+  );
 };
--- scripts/background.js
+++ scripts/background.js
@@ -1,31 +1,61 @@
 /** @format */
 
-var authorization;
-chrome.storage.sync.get(["slackBotToken"], function (result) {
-  authorization = `Bearer ${result.slackBotToken}`;
-});
+const getStorage = async () => {
+  storage = await chrome.storage.sync.get([
+    "slackBotToken",
+    "channelToPost",
+    "messagePrefix",
+  ]);
+  const slackBotToken = storage.slackBotToken ?? "";
+  const channel = storage.channelToPost ?? "";
+  const messagePrefix = storage.messagePrefix ?? "";
+  return { slackBotToken, channel, messagePrefix };
+};
 
-chrome.action.onClicked.addListener(async (tab) => {
-  var myHeaders = new Headers();
-  myHeaders.append("Authorization", authorization);
-  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+const createRequestOptions = (slackBotToken, channel, message) => {
+  const headers = createHeaders(slackBotToken);
+  const body = createBody(message, channel);
 
-  const dataToCopy = `${tab.title}\n${tab.url}`;
-  var urlencoded = new URLSearchParams();
-  urlencoded.append("text", `気になる:\n${dataToCopy}`);
-  urlencoded.append("channel", "general");
-
-  var requestOptions = {
+  return {
     method: "POST",
-    headers: myHeaders,
-    body: urlencoded,
+    headers: headers,
+    body: body,
     redirect: "follow",
   };
+};
+
+const createHeaders = (slackBotToken) => {
+  const myHeaders = new Headers();
+  const authorization = `Bearer ${slackBotToken}`;
+  myHeaders.append("Authorization", authorization);
+  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+  return myHeaders;
+};
+
+const createBody = (message, channel) => {
+  const urlencoded = new URLSearchParams();
+  urlencoded.append("text", message);
+  urlencoded.append("channel", channel);
+
+  return urlencoded;
+};
+
+const postMessage = (slackBotToken, channel, message) => {
+  const requestOptions = createRequestOptions(slackBotToken, channel, message);
 
   fetch("https://slack.com/api/chat.postMessage", requestOptions)
     .then((response) => response.text())
     .then((result) => console.log(result))
     .catch((error) => console.log("error", error));
+};
+
+chrome.action.onClicked.addListener(async (tab) => {
+  const { slackBotToken, channel, messagePrefix } = await getStorage();
+
+  const dataToCopy = `${tab.title}\n${tab.url}`;
+  const message = `${messagePrefix}\n${dataToCopy}`;
+
+  postMessage(slackBotToken, channel, message);
 });
 
 console.log("background script is loaded!");
